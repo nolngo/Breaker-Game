d84dc464ca63ba2cd4217c2b2a4989fce4376b88
Nolan Ngo <nolanngo@protonmail.com>
2021-01-25 19:32:49 -0500

    reverted speed

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
 
-var dx = 4;
-var dy = -4;
+var dx = 2;
+var dy = -2;
 var ballRadius = 10;
 
 var color = "red"
